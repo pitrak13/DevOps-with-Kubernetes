d4ff7883dd3769039b7f9647e30de0b9d0193daf
pitrak13 <mospatrik@gmail.com>
2024-03-20 18:51:33 +0000

    introducing ConfigMaps

--- hashgen/hash-generator.py
+++ hashgen/hash-generator.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from os import environ
 import string
 import random
 import requests
 import time
 
 app = Flask(__name__)
+message = environ.get('MESSAGE', 'No MESSAGE env.var found')
 
 def hash_generator(size=10, chars=string.ascii_lowercase + string.digits):
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
@@ -13,7 +15,7 @@ def hash_generator(size=10, chars=string.ascii_lowercase + string.digits):
 @app.get('/current-status')
 async def show_current():
     pongs = requests.get("http://pingpong-svc/pingpong")
-    return hash_generator() + '<br>' + pongs.text
+    return message + '<br>' + hash_generator() + '<br>' + pongs.text
 
 
 if __name__ == '__main__':
