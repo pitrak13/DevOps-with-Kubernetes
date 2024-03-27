fcde8c8d91c7fae21bb78486e22cc63bed9f36b3
pitrak13 <mospatrik@gmail.com>
2024-03-27 18:07:26 +0000

    stateful applications

--- pingpong/ping-pong.py
+++ pingpong/ping-pong.py
@@ -1,19 +1,35 @@
+import psycopg2
+from os import environ
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "bigus digus!"
+def db_init():
+    try:
+        conn = psycopg2.connect(host="postgres-svc", user="postgres", dbname="postgres", 
+                                password=environ.get("POSTGRES_PASSWORD"), port=5432)
+        cur = conn.cursor()
+        cur.execute("""CREATE TABLE pong (count INT NOT NULL DEFAULT 0);
+                    INSERT INTO pong DEFAULT VALUES""")
+    except psycopg2.errors.DuplicateTable:
+        print("pong counter already exists")
+    except:
+        print("Connection error")
+    conn.commit()
+    cur.close()
+    return conn
 
-count = -1
-@app.get("/pingpong")
+@app.get("/pong")
 def get_pongs():
-    global count
-    count+=1
-    return (f"ping / pongs: {count}")
+    cur = conn.cursor()
+    cur.execute("""SELECT count FROM pong""")
+    pongs = cur.fetchone()
+    cur.execute("""UPDATE pong
+                    SET count = count + 1""")
+    conn.commit()
+    cur.close()
+    return "pong " + str(pongs[0])
 
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7999)
+if __name__ == "__main__":
+    conn = db_init()
+    app.run(host="0.0.0.0", port=7999)
